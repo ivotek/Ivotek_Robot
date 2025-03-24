01a06a6454afef9d84e839a24983fbf963f1721f
Daniele Loddo <ivotek@gmail.com>
2025-03-24 08:33:20 +0100

    First commit
    Robotic library base

---
+++ Ivotek_Robot.cpp
@@ -0,0 +1,380 @@
+//*********************************************************
+//Programma test per il controllo dei robot realizzati da Daniele Loddo alias Ivotek
+//
+//Realizzato da Daniele Loddo alias Ivotek
+//data: 25/08/2024
+//Versione 0.0.1
+//PSito web: www.ivotek.it
+//
+//
+//PIN per Snail
+//PIN 5 - 6  Motore sinistro
+//PIN 9 - 10 Morore destro
+//PIN A3 Sensore sinistro esterno
+//PIN A2 Sensore sinistro interno
+//PIN A1 Sensore destro interno
+//PIN A0 Sensore destro esterno
+//
+//PIN per Poor V2
+//PIN A0 controllo batteria
+//PIN 3 Trigger ultrasuoni
+//PIN 11 Ultrasuoni sinistro
+//PIN 12 Ultrasuoni centrale
+//PIN 13 Ultrasuoni destro
+//PIN 4 Linea DX
+//PIN 7 Linea SX
+//PIN 2 Linea CDX
+//PIN 8 Linea CSX
+//PIN A1 fotoresistenza sinistra
+//PIN A2 fotoresistenza centrale
+//PIN A3 fotoresistenza destra
+
+// PIN per Explorer
+//PIN 5 - 6  Motore sinistro
+//PIN 9 - 10 Morore destro
+//PIN 3 Trigger ultrasuoni
+//PIN 11 Ultrasuoni sinistro
+//PIN 12 Ultrasuoni centrale
+//PIN 13 Ultrasuoni destro
+//PIN A1 fotoresistenza sinistra
+//PIN A2 fotoresistenza centrale
+//PIN A3 fotoresistenza destra
+//*********************************************************
+
+#include "Ivotek_Robot.h"
+
+String robotName = "";
+
+void Ivotek_Robot::initialization(String name)
+{
+    robotName = name;
+
+    if(robotName == "default" || robotName == "snail" || robotName == "explorer" )
+    {
+        pinMode(5,OUTPUT);
+        pinMode(6,OUTPUT);
+        pinMode(9,OUTPUT);
+        pinMode(10,OUTPUT);
+    }
+
+    if(robotName=="snail")
+    {
+        pinMode(A0,INPUT);
+        pinMode(A1,INPUT);
+        pinMode(A2,INPUT);
+        pinMode(A3,INPUT);
+    }
+
+    if(robotName=="poor")
+    {
+        pinMode(5,OUTPUT);
+        pinMode(6,OUTPUT);
+        pinMode(9,OUTPUT);
+        pinMode(10,OUTPUT);
+        pinMode(A0,INPUT);
+        pinMode(7,OUTPUT);
+        pinMode(2,OUTPUT);
+        pinMode(8,OUTPUT);
+        pinMode(A1,INPUT);
+        pinMode(A2,INPUT);
+        pinMode(A3,INPUT);
+    }
+    
+    if(robotName == "explorer")
+    {
+        pinMode(4,INPUT);
+        pinMode(7,INPUT);
+        pinMode(2,INPUT);
+        pinMode(8,INPUT);
+        pinMode(A1,INPUT);
+        pinMode(A2,INPUT);
+        pinMode(A3,INPUT);
+    }
+
+}
+
+void Ivotek_Robot::forwards(void)
+{
+    if(robotName == "default" || robotName == "explorer")
+    {
+        analogWrite(5,255);
+        analogWrite(6,LOW);
+        analogWrite(9,255);
+        analogWrite(10,LOW);
+    }
+
+}
+
+void Ivotek_Robot::backwards(void)
+{
+    if(robotName == "default" || robotName == "explorer")
+    {
+        analogWrite(5,LOW);
+        analogWrite(6,255);
+        analogWrite(9,LOW);
+        analogWrite(10,255);
+    }
+
+}
+
+void Ivotek_Robot::turnRight(void)
+{
+    if(robotName == "default" || robotName == "explorer")
+    {
+         analogWrite(5,255);
+         analogWrite(6,LOW);
+         analogWrite(9,LOW);
+         analogWrite(10,255);
+    }
+
+}
+void Ivotek_Robot::turnLeft(void)
+{
+    if(robotName == "default" || robotName == "explorer")
+    {
+        analogWrite(5,LOW);
+        analogWrite(6,255);
+        analogWrite(9,255);
+        analogWrite(10,LOW);
+    }
+
+}
+
+void Ivotek_Robot::forwards(int speed)
+{
+    if(robotName == "default" || robotName == "explorer")
+    {
+        analogWrite(5,speed);
+        analogWrite(6,LOW);
+        analogWrite(9,speed);
+        analogWrite(10,LOW);
+    }
+
+}
+void Ivotek_Robot::backwards(int speed)
+{
+    if(robotName == "default" ||robotName == "explorer")
+    {
+        analogWrite(5,LOW);
+        analogWrite(6,speed);
+        analogWrite(9,LOW);
+        analogWrite(10,speed);
+    }
+
+}
+
+void Ivotek_Robot::stop(void)
+{
+    if(robotName == "default" || robotName == "explorer")
+    {
+        analogWrite(5,LOW);
+        analogWrite(6,LOW);
+        analogWrite(9,LOW);
+        analogWrite(10,LOW);
+    }
+
+}
+
+
+void Ivotek_Robot::forwards(int speed, int pin) {}
+void Ivotek_Robot::backwards(int speed, int pin) {}
+
+void Ivotek_Robot::button(bool direction) {}
+
+void Ivotek_Robot::analog(double threshold) {}
+
+int Ivotek_Robot::compass() {}
+int Ivotek_Robot::compass(int pin) {}
+int Ivotek_Robot::accelerometer() {}
+int Ivotek_Robot::accelerometer(int pin) {}
+
+double Ivotek_Robot::ultrasound(int trigger, int ultrasound)
+{
+    double tempoEcho = 0;
+
+    //I generate a 10 micro second pulse for the trigger
+    digitalWrite(trigger, LOW );     //Reset output
+    delayMicroseconds(3);		    //Wait 3 microseconds
+    digitalWrite(trigger, HIGH );    //Up level
+    delayMicroseconds( 10 );          //Wait 10 microseconds
+    digitalWrite(trigger, LOW );     //Reset output
+    //Get the sensor time
+    tempoEcho = pulseIn( ultrasound, HIGH,38000 );
+    delay(10);
+
+    //Convert the time in centimeter
+    return 0.034 * tempoEcho / 2;
+
+}
+
+double Ivotek_Robot::ultrasoundSx()
+{
+
+    if(robotName=="poor")
+    {
+        return Ivotek_Robot::ultrasound(3, 11);
+    }
+}
+
+double Ivotek_Robot::ultrasoundCx()
+{
+    if(robotName=="poor")
+    {
+        return Ivotek_Robot::ultrasound(3, 12);
+    }
+}
+double Ivotek_Robot::ultrasoundDx()
+{
+    if(robotName=="poor")
+    {
+        return Ivotek_Robot::ultrasound(3, 13);
+    }
+}
+
+bool Ivotek_Robot::ultrasound (int trigger, int ultrasound, double threshold)
+{
+    double distance = 0;
+    distance = Ivotek_Robot::ultrasound(trigger, ultrasound);
+
+    return distance < threshold?true:false;
+}
+
+bool Ivotek_Robot::ultrasoundSx (double threshold)
+{
+    double distance = 0;
+    distance = ultrasoundSx();
+
+    return distance < threshold?true:false;
+}
+
+bool Ivotek_Robot::ultrasoundCx (double threshold)
+{
+    double distance = 0;
+    distance = ultrasoundCx();
+
+    return distance < threshold?true:false;
+}
+
+bool Ivotek_Robot::ultrasoundDx (double threshold)
+{
+    double distance = 0;
+    distance = ultrasoundDx();
+
+    return distance < threshold?true:false;
+}
+
+double Ivotek_Robot::light(int pin)
+{
+    double vFoto = 0;
+    vFoto = analogRead(pin);
+
+    //Li converto in volt
+    return vFoto * (5.0 / 1024.0);
+
+}
+
+double Ivotek_Robot::lightSx()
+{
+    if(robotName=="poor")
+    {
+        return light(A1);
+    }
+}
+
+double Ivotek_Robot::lightCx()
+{
+    if(robotName=="poor")
+    {
+        return light(A2);
+    }
+}
+
+double Ivotek_Robot::lightDx()
+{
+    if(robotName=="poor")
+    {
+        return light(A3);
+    }
+}
+
+bool Ivotek_Robot::light(int pin, double threshold)
+{
+    double vFoto = 0;
+    vFoto = light(pin);
+
+    return vFoto < threshold ? true: false;
+}
+
+bool Ivotek_Robot::lightSx(double threshold)
+{
+    if(robotName=="poor")
+    {
+        return light(A1,threshold);
+    }
+}
+
+bool Ivotek_Robot::lightCx(double threshold)
+{
+    if(robotName=="poor")
+    {
+        return light(A2,threshold);
+    }
+}
+bool Ivotek_Robot::lightDx(double threshold)
+{
+    if(robotName=="poor")
+    {
+        return light(A3,threshold);
+    }
+}
+
+double Ivotek_Robot::battery()
+{
+    if(robotName=="poor")
+    {
+        return battery(A0);
+    }
+
+    return 0;
+}
+
+double Ivotek_Robot::battery(int pin)
+{
+    double alim_digit = 0;
+    double alimentazione = 0;
+    //I take the data from the pin port to know the power level
+    alim_digit = analogRead(pin);
+    //Convert the binary code in analogic code
+    alimentazione= alim_digit * (5.0 / 1024.0);
+    //Multiply by two to get the battery pack voltage
+    return alimentazione * 2;
+}
+
+bool Ivotek_Robot::battery(double threshold)
+{
+    double trigger = 0;
+    if(robotName=="poor")
+    {
+        trigger = battery(A0);
+    }
+
+    return trigger < threshold?true:false;
+
+}
+
+
+bool Ivotek_Robot::battery(int pin, double threshold)
+{
+    double trigger = 0;
+
+    trigger = battery(pin);
+
+    return trigger < threshold?true:false;
+}
+
+int Ivotek_Robot::temperature() {}
+int Ivotek_Robot::temperature(int pin) {}
+bool Ivotek_Robot::temperature(int pin, double threshold) {}
+int Ivotek_Robot::humidity() {}
+int Ivotek_Robot::humidity(int pin) {}
+bool Ivotek_Robot::humidity(int pin, double threshold) {}
---
+++ Ivotek_Robot.h
@@ -0,0 +1,73 @@
+//Includiamo la libreria se non è già stata inclusa
+//da un altra libreria
+#ifndef Ivotek_Robot_H
+#define Ivotek_Robot_H
+
+//Includiamo la libreria di Arduino
+#include "Arduino.h"
+
+class Ivotek_Robot
+{
+private:
+    byte pinGlobal;  // Pin dove è collegato il led
+
+public:
+    void initialization(String name);
+
+    void forwards();
+    void backwards();
+    void turnRight();
+    void turnLeft();
+    void stop();
+
+    void forwards(int speed);
+    void backwards(int speed);
+    void forwards(int speed, int pin);
+    void backwards(int speed, int pin);
+
+    void button(bool direction);
+    void analog(double threshold);
+
+    int compass();
+    int compass(int pin);
+    int accelerometer();
+    int accelerometer(int pin);
+
+
+    double ultrasound(int trigger, int ultrasound);
+    bool ultrasound (int trigger, int ultrasound, double threshold);
+    double ultrasoundSx();
+    bool ultrasoundSx(double threshold);
+    double ultrasoundDx();
+    bool ultrasoundDx(double threshold);
+    double ultrasoundCx();
+    bool ultrasoundCx(double threshold);
+
+
+    double lightSx();
+    double lightCx();
+    double lightDx();
+    double light(int pin);
+    bool light(int pin, double threshold);
+    bool lightSx(double threshold);
+    bool lightCx(double threshold);
+    bool lightDx(double threshold);
+
+    double battery();
+    double battery(int pin);
+    bool battery(double threshold);
+    bool battery(int pin, double threshold);
+
+    int temperature();
+    int temperature(int pin);
+    bool temperature(int pin, double threshold);
+    int humidity();
+    int humidity(int pin);
+    bool humidity(int pin, double threshold);
+
+
+
+
+};
+
+#endif // Ivotek_Robot_H
